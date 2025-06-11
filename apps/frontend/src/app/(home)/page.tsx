8ee6f744d6ea4da949010c04fd18fcb82d75057e
CingDam <mmd011375@gmail.com>
2025-06-11 15:07:35 +0900

    fix:병합 버그 수정

--- apps/frontend/src/app/(home)/page.tsx
+++ apps/frontend/src/app/(home)/page.tsx
@@ -1,35 +1,9 @@
+import Index from "@/components/Index";
+
 export default function main() {
     return (
-<<<<<<< Updated upstream
-        <div className="h-[calc(100%-55px)] pt-[55px]">
-            <div className="w-[95%] h-[480px] mt-3 mx-auto">
-                <img className="w-full h-full object-cover object-[center_top]" src="/banner/banner01.png"></img>
-            </div>
-            <ul className="flex space-x-6 justify-center mt-6.5">
-                <li>
-                    <div>상의/블라우스</div>
-                </li>
-                <li>
-                    <div>원피스</div>
-                </li>
-                <li>
-                    <div>바지</div>
-                </li>
-                <li>
-                    <div>스커트</div>
-                </li>
-                <li>
-                    <div>아우터</div>
-                </li>
-                <li>
-                    <div>악세서리</div>
-                </li>
-            </ul>
-        </div>
-=======
         <div className="min-h-screen flex flex-col pt-[55px] bg-white">
             <Index/>
         </div>  
->>>>>>> Stashed changes
     )
 }
