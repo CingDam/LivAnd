d9865dc2036e78791ec7c38275c3abac2d7c4c8d
shineunji1 <sej980130@gmail.com>
2025-06-16 23:47:03 +0900

    소셜로그인 카카오, 네이버

--- apps/server/src/modules/auth.module.ts
+++ apps/server/src/modules/auth.module.ts
@@ -1,7 +1,14 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthController } from "src/auth/auth.controller";
+import { User } from "src/user/user.entity";
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([User]), // 👈 요거 추가
+        JwtModule.register({ secret: 'your_jwt_secret' }) // 임시로 secret 설정 (실제는 .env에서 가져오기)
+    ],
     controllers: [AuthController],
     providers:[]
 })
